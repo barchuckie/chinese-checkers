3904c1d82b164dbda41e8bd43bfebf12382f3238
Smaczny <smaczny00@gmail.com>
2018-12-07 13:41:23 +0100

    Test

---
+++ src/test/java/DisplayBoardTest.java
@@ -0,0 +1,11 @@
+import com.chinesecheckers.client.StandardBoardView;
+import org.junit.Test;
+
+public class DisplayBoardTest {
+    @Test
+    public void testDisplayBoard()
+    {
+        StandardBoardView standardBoardView = new StandardBoardView();
+        standardBoardView.start();
+    }
+}
