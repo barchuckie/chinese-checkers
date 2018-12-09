1b7df7a5ac87f6c73edcbd95aa4b58d1ae9cc881
Smaczny <smaczny00@gmail.com>
2018-12-09 21:56:35 +0100

    uzupełniłem porty w playerze, bo nie było

--- src/test/java/StandardBoardGeneratorTest.java
+++ src/test/java/StandardBoardGeneratorTest.java
@@ -5,40 +5,61 @@ import com.chinesecheckers.server.board.Field;
 import com.chinesecheckers.server.board.StandardBoard.StandardBoardGenerator;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class StandardBoardGeneratorTest {
     @Test
     public void testStandardBoardCreating() {
-        int numOfPlayers = 3;
-        Player player1 = new Player("pat");
-        Player player2 = new Player("mat");
-        Player player3 = new Player("sat");
+        try
+        {
+            int numOfPlayers = 3;
+            Player player1 = new Player("pat", new Socket("127.0.0.1", 8901));
+            Player player2 = new Player("mat", new Socket("127.0.0.1", 8901));
+            Player player3 = new Player("sat", new Socket("127.0.0.1", 8901));
 
-        Player[] players = { player1, player2, player3 };
-        GameMode mode = new StandardGameMode();
+            Player[] players = {player1, player2, player3};
+            GameMode mode = new StandardGameMode();
 
-        GameData data = new GameData(mode, numOfPlayers,players);
+            GameData data = new GameData(mode, numOfPlayers, players);
 
-        BoardGenerator standardBoardGenerator = new StandardBoardGenerator();
-        Board standardBoard = standardBoardGenerator.generateBoard(data);
-        Field[][] fields = standardBoard.getFields();
+            BoardGenerator standardBoardGenerator = new StandardBoardGenerator();
+            Board standardBoard = standardBoardGenerator.generateBoard(data);
+            Field[][] fields = standardBoard.getFields();
 
-        for(int i  = 0; i < fields.length; i++) {
-            for (Field field : fields[i]) {
-                if(field != null) {
-                    if(field.getPlayer() == null){
-                        System.out.print('*');
-                    } else if(field.getPlayer().getNick().equals("pat")) {
-                        System.out.print('P');
-                    } else if(field.getPlayer().getNick().equals("mat")) {
-                        System.out.print('M');
-                    } else if(field.getPlayer().getNick().equals("sat")) {
-                        System.out.print('S');
+            for (int i = 0; i < fields.length; i++)
+            {
+                for (Field field : fields[i])
+                {
+                    if (field != null)
+                    {
+                        if (field.getPlayer() == null)
+                        {
+                            System.out.print('*');
+                        }
+                        else if (field.getPlayer().getNick().equals("pat"))
+                        {
+                            System.out.print('P');
+                        }
+                        else if (field.getPlayer().getNick().equals("mat"))
+                        {
+                            System.out.print('M');
+                        }
+                        else if (field.getPlayer().getNick().equals("sat"))
+                        {
+                            System.out.print('S');
+                        }
                     }
-                } else {
-                    System.out.print(' ');
+                    else
+                    {
+                        System.out.print(' ');
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
+        }catch(IOException ex)
+        {
+            ex.printStackTrace();
         }
     }
 }
